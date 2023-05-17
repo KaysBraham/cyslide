a3cb68bbb0afe18cad1e7561c256a84578201fba
bolamigo <mendynicol@cy-tech.fr>
2023-05-17 19:02:18 +0200

    Fix : Move : multiple empty squares allowed
    Signed-off-by: bolamigo <mendynicol@cy-tech.fr>

--- src/Move.java
+++ src/Move.java
@@ -1,7 +1,5 @@
 package src;
 
 public class Move {
-    // no need for the previous position, only one possibility
-    private int tileMovedX;
-    private int tileMovedY;
+    private int newX, newY, previousX, previousY;
 }
