65d55d5488df15b11999951d2d9a8610a18999d5
bolamigo <mendynicol@cy-tech.fr>
2023-05-17 13:40:09 +0200

    Minor fix : Chronometer : default elapsedTime = 0
    Signed-off-by: bolamigo <mendynicol@cy-tech.fr>

--- src/Chronometer.java
+++ src/Chronometer.java
@@ -1,7 +1,7 @@
 package src;
 public class Chronometer {
     private int startTime; // Holds the starting time in seconds
-    private int elapsedTime; // Holds the elapsed time in seconds
+    private int elapsedTime = 0; // Holds the elapsed time in seconds
     private boolean running = false; // Indicates whether the chronometer is running or not
     
     public int getStartTime() {
